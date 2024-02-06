c0adc6da676de201c80ced0fe2262bf26f1d766d
meredithbevill <meredithbevill@gmail.com>
2024-02-05 21:08:13 -0600

    Homework1.2

---
+++ Homework1.2/sketch.js
@@ -0,0 +1,35 @@
+//define array of colors 
+let colors = ['#FF0000', '#FFA500', '#FFFF00', '#008000', '#00FFFF', '#0000FF', '#FF00FF', '#A52A2A', '#FFFFFF', '#000000'];
+//initialize current color as black
+let currentColor = '#000000';
+
+function setup() {
+  createCanvas(1500, 900);
+  drawColorPalette();
+}
+
+function draw() {
+  // check if mouse is pressed within boundaries
+  if (mouseIsPressed && mouseX < width && mouseY < height) {
+    strokeWeight(5);
+    stroke(currentColor);
+    line(pmouseX, pmouseY, mouseX, mouseY);
+  }
+}
+
+function drawColorPalette() {
+  // Draw color palette on the left side
+  for (let i = 0; i < colors.length; i++) {
+    fill(colors[i]);
+    rect(0, i * 65, 65, 65);
+  }
+}
+
+function mousePressed() {
+  // Check if the mouse is pressed on the color palette
+  if (mouseX < 65 && mouseY < height) {
+    // Determine the selected color from the palette
+    let colorIndex = int(mouseY / 65);
+    currentColor = colors[colorIndex];
+  }
+}
